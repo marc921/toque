122ba4203c6a87019f53a311604d083d7d33d492
Marc Brun <marc.brun.921@gmail.com>
2024-02-26 23:10:29 +0100

    feat(scanner): showcase nmap in scanner

--- cmd/scanner/go.mod
+++ cmd/scanner/go.mod
@@ -3,20 +3,18 @@ module marcbrun.io/toque/scanner
 go 1.22
 
 require (
-	github.com/labstack/echo/v4 v4.11.4
+	github.com/Ullaakut/nmap/v3 v3.0.2
 	marcbrun.io/toque/pkg v0.0.0-00010101000000-000000000000
 )
 
 require (
-	github.com/labstack/gommon v0.4.2 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.3 // indirect
 	github.com/rabbitmq/amqp091-go v1.9.0 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
-	golang.org/x/net v0.19.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
 
--- cmd/scanner/scanner.go
+++ cmd/scanner/scanner.go
@@ -2,38 +2,71 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"time"
 
 	"marcbrun.io/toque/pkg"
+
+	"github.com/Ullaakut/nmap/v3"
 )
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	go pkg.OnSignal(cancel)
 
-	// Create RabbitMQ publisher
-	publisher, err := pkg.NewRabbitMQClient()
+	// Equivalent to `/usr/local/bin/nmap -p 80,443,843 google.com facebook.com youtube.com`,
+	scanner, err := nmap.NewScanner(
+		ctx,
+		nmap.WithTargets("google.com", "facebook.com", "youtube.com"),
+		nmap.WithPorts("80,443,843"),
+	)
 	if err != nil {
-		log.Fatal(fmt.Errorf("failed to create RabbitMQ publisher: %w", err))
+		log.Fatalf("unable to create nmap scanner: %v", err)
 	}
-	defer publisher.Close()
 
-	ticker := time.NewTicker(2 * time.Second)
-	for {
-		select {
-		case <-ctx.Done():
-			if ctx.Err() != context.Canceled {
-				log.Printf("context error: %v", ctx.Err())
-			}
-			return
-		case <-ticker.C:
-			err = publisher.Publish(ctx, fmt.Sprint(time.Now().UnixNano()))
-			if err != nil {
-				log.Printf("failed to publish a message: %v", err)
-			}
+	result, warnings, err := scanner.Run()
+	if len(*warnings) > 0 {
+		log.Printf("run finished with warnings: %s\n", *warnings) // Warnings are non-critical errors from nmap.
+	}
+	if err != nil {
+		log.Fatalf("unable to run nmap scan: %v", err)
+	}
+
+	// Use the results to print an example output
+	for _, host := range result.Hosts {
+		if len(host.Ports) == 0 || len(host.Addresses) == 0 {
+			continue
+		}
+
+		log.Printf("Host %q:\n", host.Addresses[0])
+
+		for _, port := range host.Ports {
+			log.Printf("\tPort %d/%s %s %s\n", port.ID, port.Protocol, port.State, port.Service.Name)
 		}
 	}
+
+	log.Printf("Nmap done: %d hosts up scanned in %.2f seconds\n", len(result.Hosts), result.Stats.Finished.Elapsed)
+
+	// // Create RabbitMQ publisher
+	// publisher, err := pkg.NewRabbitMQClient()
+	// if err != nil {
+	// 	log.Fatal(fmt.Errorf("failed to create RabbitMQ publisher: %w", err))
+	// }
+	// defer publisher.Close()
+
+	// ticker := time.NewTicker(2 * time.Second)
+	// for {
+	// 	select {
+	// 	case <-ctx.Done():
+	// 		if ctx.Err() != context.Canceled {
+	// 			log.Printf("context error: %v", ctx.Err())
+	// 		}
+	// 		return
+	// 	case <-ticker.C:
+	// 		err = publisher.Publish(ctx, fmt.Sprint(time.Now().UnixNano()))
+	// 		if err != nil {
+	// 			log.Printf("failed to publish a message: %v", err)
+	// 		}
+	// 	}
+	// }
 
 }
